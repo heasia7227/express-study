e8414d10181597d5f7118f88c24b090eeb9f6ee7
heasia7227 <108118358+heasia7227@users.noreply.github.com>
2023-02-09 17:03:39 +0800

    swagger annotation

--- demo5/src/controllers/index.ts
+++ demo5/src/controllers/index.ts
@@ -4,14 +4,9 @@ import goodsController from "./goods";
 
 const controller = express.Router();
 
-controller.use(usersController, goodsController);
+controller.use("/users", usersController);
+// controller.use(usersController, goodsController);
 
-/**
- * @route GET /
- * @group root path
- * @returns {object} 200 - Welcome to visit.
- * @returns {Error}  default - Unexpected error
- */
 controller.get("/", function (req: Request, res: Response, next: NextFunction) {
     res.json({
         code: 200,
--- demo5/src/controllers/users.ts
+++ demo5/src/controllers/users.ts
@@ -3,12 +3,37 @@ import UserService from "../services/user";
 
 const controller = express.Router();
 
-controller.get("/users", function (req: Request, res: Response, next: NextFunction) {
+/**
+ * @route POST /users/login
+ * @group user
+ * @param {string} username.query.required - username
+ * @param {string} password.query.required - user's password.
+ * @returns {object} 200 - verify result
+ * @returns {Error}  default - Unexpected error
+ */
+controller.post("/login", function (req: Request, res: Response, next: NextFunction) {
     UserService.login();
 
     res.json({
         code: 200,
         data: "user login.",
+    });
+});
+
+/**
+ * @route GET /users/{userId}
+ * @group user
+ * @param {integer} userId.path.required - userId
+ * @returns {object} 200 - verify result
+ * @returns {Error}  default - Unexpected error
+ */
+controller.get("/:userId", function (req: Request, res: Response, next: NextFunction) {
+    console.log("req.params: ", req.params);
+    console.log("req.query: ", req.query);
+
+    res.json({
+        code: 200,
+        data: "user find.",
     });
 });
 
